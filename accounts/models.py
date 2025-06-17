4125947a63a454041b09147580905c2715628813
junaid <junaidkaratt36@gmail.com>
2025-06-17 17:36:41 +0530

    Django project setup

---
+++ accounts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+import enum
+
+class UserRole(enum.Enum):
+    JOBSEEKER="jobseeker"
+    EMPLOYER="employer"
+
+
+class User(AbstractUser):
+    ROLE_CHOICES=[
+        ( UserRole.JOBSEEKER.value, 'job Seeker'),
+        ( UserRole.EMPLOYER.value, 'Employer'),
+    ]
+
+
+    username= None
+    email= models.EmailField(unique=True)
+    full_name= models.CharField(max_length=100, null=True)
+    phone_number= models.CharField(max_length=15, null=True)
+    roles= models.CharField(max_length=10, choices=ROLE_CHOICES, default="")
+    is_verified= models.BooleanField(default= False)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['full_name']
+
+    objects= CustomUserManager()
+
+    def __str__(self):
+        return f"{self.fullname} ({self.get_role_display()})"
+    
+
+
+
+# Create your models here.
